5035d4f59a7217ed0a8162b9bcf816b894e8f6e6
kinfi4 <ilyamakarov223@gmail.com>
2023-05-30 20:49:07 +0300

    ALL TPO2

---
+++ MPI2/src/WordsAvgLengthFinished.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ForkJoinPool;
+
+class WordLengthCalculator extends RecursiveTask<long[]> {
+    private final ArrayList<String> list;
+    private final int start;
+    private final int end;
+    private static final int THRESHOLD = 50;
+
+    public WordLengthCalculator(ArrayList<String> list, int start, int end) {
+        this.list = list;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected long[] compute() {
+        int length = end - start;
+        if (length <= THRESHOLD) {
+            return calculate();
+        } else {
+            WordLengthCalculator firstSubTask = new WordLengthCalculator(list, start, start + length/2);
+            WordLengthCalculator secondSubTask = new WordLengthCalculator(list, start + length/2, end);
+            firstSubTask.fork();
+            long[] secondSubResult = secondSubTask.compute();
+            long[] firstSubResult = firstSubTask.join();
+            return new long[] {firstSubResult[0] + secondSubResult[0], firstSubResult[1] + secondSubResult[1]};
+        }
+    }
+
+    private long[] calculate() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += list.get(i).length();
+        }
+        return new long[] {sum, end-start};
+    }
+}
+
+public class WordsAvgLengthFinished {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+            File file = new File("path_to_your_file.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                list.add(word);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ForkJoinPool pool = new ForkJoinPool();
+        WordLengthCalculator task = new WordLengthCalculator(list, 0, list.size());
+        long[] result = pool.invoke(task);
+        double average = 1.0*result[0]/result[1];
+
+        System.out.println("Average word length: " + average);
+    }
+}
